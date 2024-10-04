30280ff90f0054ce0fa3a4c667a58b722625588c
Abhishek Jain <erabhishek17jain@gmail.com>
2024-10-04 11:38:39 +0530

    queue type

--- app/(dashboard)/queue/page.tsx
+++ app/(dashboard)/queue/page.tsx
@@ -90,7 +90,7 @@ export default function Queue() {
                                         key={queue._id.$oid.toString()}
                                         className='odd:bg-white even:bg-gray-50 border-b cursor-pointer hover:bg-gray-100'
                                         onClick={() => {
-                                            router.push(`${campaignType}/active/report/${queue.campaign._id.$oid.toString()}`);
+                                            router.push(`${queue.campaign.type}/${queue.campaignStatus}/report/${queue.campaign._id.$oid.toString()}`);
                                         }}>
                                         <th scope='row' className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
                                             {queue?.campaign?.title}
