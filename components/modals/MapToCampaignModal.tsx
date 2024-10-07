e564b34f19a88682ab4eb4b211731f41899807c6
Abhishek Jain <erabhishek17jain@gmail.com>
2024-10-07 10:41:37 +0530

    issue fixes

--- components/modals/MapToCampaignModal.tsx
+++ components/modals/MapToCampaignModal.tsx
@@ -54,7 +54,7 @@ export default function MapToCampaignModal({ orgsId, platform, profileIds, openC
     useLayoutEffect(() => {
         setIsLoading(true);
         CampaignNetworkService.instance
-            .getCampaigns(1, 100, 'all', 'all', '', 'influencer')
+            .getCampaigns(1, 100, 'active', 'all', '', 'influencer')
             .then((res) => {
                 setIsLoading(false);
                 setCampaigns(res.data);
